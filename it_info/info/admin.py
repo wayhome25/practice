90e6359bb2d85101699b28613f004ed7e753ba64
wayhome25 <siwabada@gmail.com>
2017-05-25 00:02:15 +0900

    info.Info/Category 모델생성 및 admin 등록 urls.py 오타수정

--- it_info/info/admin.py
+++ it_info/info/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Info, Category
 
-# Register your models here.
+@admin.register(Info)
+class InfoAdmin(admin.ModelAdmin):
+	list_display = ['event_name', 'event_date','author', 'fee']
+	list_display_links = ['event_name']
+
+admin.site.register(Category)
--- it_info/info/models.py
+++ it_info/info/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Info(models.Model):
+	event_name = models.CharField('행사명', max_length=100)
+	event_date = models.DateTimeField('행사일시')
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='작성자')
+	category_set = models.ManyToManyField('Category', '분야')
+	fee = models.CharField('참가비', max_length=10, choices = (
+		('free', '무료'),
+		('paid', '유료'),
+		))
+	location = models.CharField('행사장소', max_length=200)
+	link = models.URLField('링크', blank=True)
+	description = models.TextField('행사내용')
+	created_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.event_name
+
+class Category(models.Model):
+	name = models.CharField(max_length=20, unique=True)
+
+	def __str__(self):
+		return self.name
